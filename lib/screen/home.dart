2ef4a12f36cb25eaf43170c1e0171b55af31014b
AbdiwakB <abdiwakbek3226@gmail.com>
2023-11-15 07:07:53 +0300

    Profile Avatar updated

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -37,21 +37,37 @@ class _HomeState extends State<Home> {
       builder: (context) {
         return AlertDialog(
           title: const Text("Select Image Source"),
-          content: ListView(
-            children: [
-              ListTile(
-                leading: Icon(Icons.image),
-                title: Text("Gallary"),
-                onTap: pickImageGallery,
-              ),
-              ListTile(
-                leading: Icon(Icons.camera),
-                title: Text("Camera"),
-                onTap: pickImageCamera,
-              ),
-            ],
+          content: Container(
+            height: 200,
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.image),
+                  title: Text("Gallary"),
+                  onTap: () {
+                    pickImageGallery();
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.camera),
+                  title: Text("Camera"),
+                  onTap: () {
+                    pickImageCamera();
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
           ),
-          actions: [],
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Close"),
+            )
+          ],
         );
       },
     );
@@ -59,41 +75,60 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.black, width: 2)),
-                  child: CircleAvatar(
-                    radius: 80,
-                    backgroundImage:
-                        (image != null) ? FileImage(File(image!.path)) : null,
-                  ),
-                ),
-                Positioned(
-                  bottom: 5,
-                  right: 10,
-                  child: Icon(
-                    Icons.image,
-                    size: 30,
-                  ),
-                )
-              ],
-            ),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: Center(child: Text("Edit Profile")),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showImageOption(context);
-        },
-        child: Icon(Icons.image),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.black, width: 2)),
+                    child: CircleAvatar(
+                      radius: 80,
+                      backgroundImage:
+                          (image != null) ? FileImage(File(image!.path)) : null,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 5,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.green[700],
+                        shape: BoxShape.circle,
+                      ),
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.camera_alt,
+                          size: 25,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          showImageOption(context);
+                        },
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showImageOption(context);
+          },
+          child: Icon(Icons.image),
+        ),
       ),
     );
   }
