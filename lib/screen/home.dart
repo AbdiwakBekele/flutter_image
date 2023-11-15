7447ead1e9281cd611e48fdc5b72ee663d9f2bbf
AbdiwakB <abdiwakbek3226@gmail.com>
2023-11-15 05:05:17 +0300

    image source gallery added

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -9,14 +11,19 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  XFile? image;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("testing"),
+        child: image != null ? Image.file(File(image!.path)) : Text("No Image"),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () async {
+          ImagePicker picker = new ImagePicker();
+          image = await picker.pickImage(source: ImageSource.gallery);
+          setState(() {});
+        },
         child: Icon(Icons.image),
       ),
     );
