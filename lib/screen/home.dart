09d7650ff2247a350db5389a7011c1c63bba604a
AbdiwakB <abdiwakbek3226@gmail.com>
2023-11-15 06:24:54 +0300

    Image Avatar included

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -11,18 +11,87 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  ImagePicker picker = new ImagePicker();
   XFile? image;
+
+  // From Gallery
+  Future<void> pickImageGallery() async {
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      image = pickedImage;
+    });
+  }
+
+  // From Camera
+  Future<void> pickImageCamera() async {
+    final pickedImageCamera =
+        await picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      image = pickedImageCamera;
+    });
+  }
+
+  void showImageOption(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Select Image Source"),
+          content: ListView(
+            children: [
+              ListTile(
+                leading: Icon(Icons.image),
+                title: Text("Gallary"),
+                onTap: pickImageGallery,
+              ),
+              ListTile(
+                leading: Icon(Icons.camera),
+                title: Text("Camera"),
+                onTap: pickImageCamera,
+              ),
+            ],
+          ),
+          actions: [],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: image != null ? Image.file(File(image!.path)) : Text("No Image"),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.black, width: 2)),
+                  child: CircleAvatar(
+                    radius: 80,
+                    backgroundImage:
+                        (image != null) ? FileImage(File(image!.path)) : null,
+                  ),
+                ),
+                Positioned(
+                  bottom: 5,
+                  right: 10,
+                  child: Icon(
+                    Icons.image,
+                    size: 30,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          ImagePicker picker = new ImagePicker();
-          image = await picker.pickImage(source: ImageSource.gallery);
-          setState(() {});
+        onPressed: () {
+          showImageOption(context);
         },
         child: Icon(Icons.image),
       ),
